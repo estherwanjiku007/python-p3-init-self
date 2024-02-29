a53655239d0111df4f3c7bfc565bd6dff3d3ab1e
estherwanjiku007 <estherwanjiqu@gmail.com>
2024-02-29 14:30:01 +0300

    ain

--- lib/dog.py
+++ lib/dog.py
@@ -1,4 +1,14 @@
 #!/usr/bin/env python3
 
 class Dog:
-    pass
+    def __init__(self,name,breed="Mutt") :
+        self.name=name
+        self.breed=breed
+    def showing_self(self):
+        print(f"Showing {self}")
+        return self
+fido=Dog("Fido")
+print(fido.name)
+snoopy=Dog("Snoopy","pet")
+print(snoopy.name,snoopy.breed)
+
